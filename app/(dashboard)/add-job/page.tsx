7252f6153bd6a7ace5ee4f234636594f90c326e0
Thomas Neos <tneos@hotmail.com>
2025-10-01 12:44:45 +0100

    Add dashboard and pages structrure, initial commit

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,4 @@
+function AddJobPage() {
+  return <h1 className="text-4xl">Add Job Page</h1>;
+}
+export default AddJobPage;
---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,4 @@
+function JobsPage() {
+  return <h1 className="text-4xl">Jobs Page</h1>;
+}
+export default JobsPage;
